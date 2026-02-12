ef0f8d1886bfd368d4e55261d8e6fbebd8842cae
sathishreddy.np <sathishreddy.np@gmail.com>
2026-02-13 02:42:05 +0530

    feat: Initialize new Laravel project with Filament, Log Viewer, and Gemini skill configurations.

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Laravel\Horizon\Horizon;
+use Opcodes\LogViewer\Facades\LogViewer;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+        LogViewer::auth(function ($request) {
+            return $request->user()
+            && in_array($request->user()->email, [
+            'it@kutoot.com',
+            ]);
+        });
+
+
+    }
+}
