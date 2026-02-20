ea89125bae8c310d900478a3c5d81fca3bc4ee54
sathishreddy.np <sathishreddy.np@gmail.com>
2026-02-20 14:59:34 +0530

    fix: Update UserFactory to use global fake() function for consistency

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,12 +23,10 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
-        $faker = $this->faker ?? \Faker\Factory::create();
-
         return [
-            'name' => $faker->name(),
-            'email' => $faker->unique()->safeEmail(),
-            'mobile' => $faker->unique()->numerify('9#########'),
+            'name' => fake()->name(),
+            'email' => fake()->unique()->safeEmail(),
+            'mobile' => fake()->unique()->numerify('9#########'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
