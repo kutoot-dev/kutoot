6fda3dc564990eceed46646906b7949e020987d5
sathishreddy.np <sathishreddy.np@gmail.com>
2026-02-20 14:49:08 +0530

    fix: Replace fake() with $this->faker in UserFactory for consistency

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,9 +24,9 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'mobile' => fake()->unique()->numerify('9#########'),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'mobile' => $this->faker->unique()->numerify('9#########'),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
