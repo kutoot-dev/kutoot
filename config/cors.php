8f6b6e59120c8b3ee61f4e8d72266bbc0b1275e0
sathishreddy.np <sathishreddy.np@gmail.com>
2026-03-11 19:10:04 +0530

    feat: Add new CORS origin for Amplify app
    - Included 'https://main.d2gkmd4xrkda1d.amplifyapp.com' in the CORS configuration to allow requests from the new Amplify application.

--- config/cors.php
+++ config/cors.php
@@ -26,6 +26,7 @@ return [
             'https://kutoot.com',
 	    'https://www.kutoot.com',
 	    'https://main.d1m3jak1924r3d.amplifyapp.com',
+        'https://main.d2gkmd4xrkda1d.amplifyapp.com',
 	    'https://sanjeev-test.d1m3jak1924r3d.amplifyapp.com',
 	    'https://kutoot-backend.s3.ap-south-1.amazonaws.com'
 	   
